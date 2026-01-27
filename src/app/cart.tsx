ba94553308df70170d77f674a89a9e4896994c25
OkCripto <criptounity@gmail.com>
2026-01-27 17:27:49 +0530

    Total Logic

--- src/app/cart.tsx
+++ src/app/cart.tsx
@@ -2,18 +2,21 @@ import CartListItem from "@/components/CartListItems";
 import { useCart } from "@/providers/CartProvider";
 import { StatusBar } from "expo-status-bar";
 import React from "react";
-import { FlatList, Platform, View } from "react-native";
+import {FlatList, Platform, Text, View} from "react-native";
+import Button from "@components/Button";
 
 const CartScreen = () => {
-  const { items } = useCart();
+  const { items, total } = useCart();
 
   return (
-    <View>
+    <View style={{padding: 10}}>
       <FlatList
         data={items}
         renderItem={({ item }) => <CartListItem cartItem={item} />}
-        contentContainerStyle={{ padding: 10, gap: 10 }}
+        contentContainerStyle={{ gap: 10 }}
       />
+        <Text style={{marginTop:20, fontSize:20, fontWeight:500}}>Total: ${total}</Text>
+        <Button text="Checkout"/>
 
       <StatusBar style={Platform.OS === "ios" ? "light" : "auto"} />
     </View>
